0ecc1563fedf7532a9fbf53afac23bfde7542383
jzeng703 <xiankang.zeng@duke.edu>
2021-01-28 16:32:36 -0500

    Create HDL driver and upate interface function

--- blood_tests.py
+++ blood_tests.py
@@ -2,9 +2,34 @@ def interface():
     print ("Blood Test Analysis")
     while True:
         print ("\nOptions")
+        print ("1 - HDL")
         print ("9 - Quit")
         choice = input ("Enter an option: ")
         if choice == "9":
             return 
+        elif choice == "1":
+            HDL_driver()
+
+def HDL_driver():
+    HDL = get_HDL_input()
+    analysis = analyze_HDL(HDL)
+    output_HDL(HDL, analysis)
+
+def get_HDL_input():
+    HDL = input("Enter HDL Level: ")
+    return int(HDL)
+
+def analyze_HDL(HDL):
+    if HDL >= 60:
+        return ("Normal")
+    elif HDL < 40:
+        return ("Low")
+    else:
+        return ("Borderline Low")
+
+def output_HDL(HDL, analysis):
+    print("The HDL entered was {}".format(HDL))
+    print("The level is {}".format(analysis))
+
 
 interface()
