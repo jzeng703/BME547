ea5ea17074d5ae6e881210651c70938b753f5eb8
jzeng703 <xiankang.zeng@duke.edu>
2021-01-28 16:54:17 -0500

    Create LDL Funtion and updated interface

--- blood_tests.py
+++ blood_tests.py
@@ -3,17 +3,21 @@ def interface():
     while True:
         print ("\nOptions")
         print ("1 - HDL")
+        print ("2 - LDL")
         print ("9 - Quit")
         choice = input ("Enter an option: ")
         if choice == "9":
             return 
         elif choice == "1":
             HDL_driver()
+        elif choice == "2":
+            LDL_driver()
+
 
 def HDL_driver():
     HDL = get_HDL_input()
-    analysis = analyze_HDL(HDL)
-    output_HDL(HDL, analysis)
+    analysis1 = analyze_HDL(HDL)
+    output_HDL(HDL, analysis1)
 
 def get_HDL_input():
     HDL = input("Enter HDL Level: ")
@@ -27,9 +31,33 @@ def analyze_HDL(HDL):
     else:
         return ("Borderline Low")
 
-def output_HDL(HDL, analysis):
+def output_HDL(HDL, analysis1):
     print("The HDL entered was {}".format(HDL))
-    print("The level is {}".format(analysis))
+    print("The level is {}".format(analysis1))
+
+
+def LDL_driver():
+    LDL = get_LDL_input()
+    analysis2 = analyze_LDL(LDL)
+    output_LDL(LDL, analysis2)
+
+def get_LDL_input():
+    LDL = input("Enter LDL Level: ")
+    return int(LDL)
+
+def analyze_LDL(LDL):
+    if LDL < 130:
+        return ("Normal")
+    elif 130 <= LDL <= 159:
+        return ("Borderline Hight")
+    elif 160 <= LDL <= 189:
+        return ("High")
+    else:
+        return ("Very High")
+
+def output_LDL(LDL, analysis2):
+    print("The LDL entered was {}".format(LDL))
+    print("The level is {}".format(analysis2))
 
 
 interface()
