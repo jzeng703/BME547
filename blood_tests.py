c9529dcf3f2fced289d49a7354292c8f9c55c7b2
jzeng703 <xiankang.zeng@duke.edu>
2021-01-28 15:55:17 -0500

    Create blood_tests.py

---
+++ blood_tests.py
@@ -0,0 +1,10 @@
+def interface():
+    print ("Blood Test Analysis")
+    while True:
+        print ("\nOptions")
+        print ("9 - Quit")
+        choice = input ("Enter an option: ")
+        if choice == "9":
+        return 
+
+interface()
