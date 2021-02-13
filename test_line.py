215c9b4e4268946e57971ddbda5bb9b940e456bf
jzeng703 <xiankang.zeng@duke.edu>
2021-02-13 15:35:15 -0500

    fixed problem in test file and developed the function

--- test_line.py
+++ test_line.py
@@ -1,11 +1,11 @@
 import pytest
 
-@pytest.mark.parametrize("(x1,y1), (x2,y2), x, expected",[
-    ((1,1), (2,2), 3, 3),
-    ((1,2), (2,4), 3, 6),
-    ((0,0), (2,0), 3, 0),
+@pytest.mark.parametrize("x1, y1, x2, y2, x, expected",[
+    (1, 1, 2, 2, 3, 3),
+    (1, 2, 2, 4, 3, 6),
+    (0, 0, 2, 0, 3, 0),
 ])
-def test_line((x1,y1), (x2,y2), x, expected):
+def test_line(x1, y1, x2, y2, x, expected):
     from two_points import line
-    answer = line((x1,y1), (x2,y2), x)
+    answer = line(x1, y1, x2, y2, x)
     assert answer == expected
